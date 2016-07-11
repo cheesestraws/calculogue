0f1bb2842592b439db85bed9482ce34f51f857fe
Camilla Berglund <elmindreda@elmindreda.org>
2016-07-11 13:30:37 +0200

    Improve debug output for C99 example driver

--- C99/driver.c
+++ C99/driver.c
@@ -54,43 +54,53 @@ static void print(TLVM* vm, const char* text)
     printf("%s", text);
 }
 
+static void print_value(FILE* stream, const TLValue* value)
+{
+    switch (value->type)
+    {
+        case TL_INTEGER:
+            fprintf(stream, "%" PRIi64, value->i);
+            break;
+        case TL_FLOAT:
+            fprintf(stream, "%f", value->f);
+            break;
+        case TL_STRING:
+        {
+            const char* c = value->s;
+
+            fprintf(stream, "\"");
+            while (*c)
+            {
+                if (*c == '\t')
+                    fprintf(stream, "\\t");
+                else if (*c == '\n')
+                    fprintf(stream, "\\n");
+                else if (*c == '\r')
+                    fprintf(stream, "\\r");
+                else if (*c == '\\')
+                    fprintf(stream, "\\\\");
+                else if (*c == '\"')
+                    fprintf(stream, "\\\"");
+                else
+                    fputc(*c, stream);
+
+                c++;
+            }
+
+            fputc('\"', stream);
+            break;
+        }
+    }
+}
+
 static void print_stack(const TLStack* stack)
 {
     printf("[");
 
     for (size_t i = 0;  i < stack->count;  i++)
     {
-        switch (stack->values[i].type)
-        {
-            case TL_INTEGER:
-                printf(" i:%" PRIi64, stack->values[i].i);
-                break;
-            case TL_FLOAT:
-                printf(" f:%f", stack->values[i].f);
-                break;
-            case TL_STRING:
-            {
-                const char* c = stack->values[i].s;
-
-                printf(" s:\"");
-                while (*c)
-                {
-                    if (*c == '\t')
-                        printf("\\t");
-                    else if (*c == '\n')
-                        printf("\\n");
-                    else if (*c == '\r')
-                        printf("\\r");
-                    else
-                        putchar(*c);
-
-                    c++;
-                }
-
-                printf("\"");
-                break;
-            }
-        }
+        printf(" ");
+        print_value(stdout, stack->values + i);
     }
 
     printf(" ]\n");
@@ -111,16 +121,19 @@ static void step(TLVM* vm)
 
 static void panic(TLVM* vm, const char* message)
 {
-    fprintf(stderr, "%s\n", message);
+    fprintf(stderr, "%s:%u: error: %s\n",
+            tl_top_file(vm),
+            tl_top_line(vm),
+            message);
     fprintf(stderr, "Call stack:\n");
 
     for (size_t i = 0;  i < vm->ccount;  i++)
     {
-        TLValue string = tl_cast_value(vm->contexts[i]->token, TL_STRING);
-        fprintf(stderr, " %s (%s:%u)\n",
-                string.s,
-                tl_file(vm, string),
-                tl_line(string));
+        fprintf(stderr, "#%zu ", i);
+        print_value(stderr, &vm->contexts[i]->token);
+        fprintf(stderr, " (%s:%u)\n",
+                tl_file(vm, vm->contexts[i]->token),
+                tl_line(vm->contexts[i]->token));
     }
 
     exit(EXIT_FAILURE);
